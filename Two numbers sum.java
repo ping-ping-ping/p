7e587b5ce34e7c87b190964328ce787e5433b5dc
ping-ping-ping <126137599+ping-ping-ping@users.noreply.github.com>
2023-09-13 08:35:46 +0800

    Create Two numbers sum.java

---
+++ Two numbers sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      Map<Integer,Integer> map=new HashMap<>();
+      for(int i=0;i<nums.length;i++){
+        map.put(nums[i],i);
+      }
+      for(int i=0;i<nums.length;i++){
+         int temp=target-nums[i];
+         if(map.containsKey(temp)&&i!=map.get(temp)){
+           return new int[]{i,map.get(temp)};
+         }
+      }
+      return new int[0];
+    }
+}
